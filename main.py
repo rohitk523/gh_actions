8c4735e6028504526d2fd35e147b98d5282b48cb
rohit-kale_ghub <rohit.kale@blueyonder.com>
2022-10-19 16:41:34 +0530

    adding multichoice

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-        '--append-action', action='append', dest="data", help="Can be data_5K, data_10K, data_15K, data_20K, data_30K, data_100K, data_500K, data_750K"
+        '--data', dest="data", help="Can be data_5K, data_10K, data_15K, data_20K, data_30K, data_100K, data_500K, data_750K"
     )
 args = parser.parse_args()
-all_lst = args.data[0].split(",")
+all_lst = args.data.split(",")
 
 
 def true_list(lst):
     res_list = [lst[i] for i in range(0, len(lst), 2) if lst[i+1] == 'true']
     return res_list
-
 
 print(true_list(all_lst))
 
