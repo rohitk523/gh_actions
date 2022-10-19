5142f534d0a37ac3892f0d1e35185631fdc536cf
rohit-kale_ghub <rohit.kale@blueyonder.com>
2022-10-19 15:02:44 +0530

    adding multichoice

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-        "--data", dest="data", help="Can be data_5K, data_10K, data_15K, data_20K, data_30K, data_100K, data_500K, data_750K", default="data_5K"
+        "--data", dest="data", type=list, help="Can be data_5K, data_10K, data_15K, data_20K, data_30K, data_100K, data_500K, data_750K", default="data_5K"
     )
 args = parser.parse_args()
 
